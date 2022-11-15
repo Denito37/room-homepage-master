926ad6a5921948c2e6fdaec0dc231cb9853096e6
Denito37 <69406178+Denito37@users.noreply.github.com>
2022-11-15 18:31:02 -0500

    html semantics, css breakpoints

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const slide1 = document.querySelector('.slide1');
 const slide2 = document.querySelector('.slide2');
 const slide3 = document.querySelector('.slide3');
+const menu = document.querySelector('.open');
 
 slide2.classList.add("hidden");
 slide3.classList.add("hidden");
@@ -12,4 +13,8 @@ function rightSwap(){
 function leftSwap(){
     slide1.classList.remove("hidden");
     slide2.classList.add("hidden");
+}
+function close(){
+    menu.style.display = "none";
+    console.log('hi');
 }
