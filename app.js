177b65b057344f80af084d7108ec8e769d2a1503
Denito37 <69406178+Denito37@users.noreply.github.com>
2022-11-20 16:12:39 -0500

    fixed slide logic

--- app.js
+++ app.js
@@ -1,20 +1,82 @@
+// * Variables
 const slide1 = document.querySelector('.slide1');
 const slide2 = document.querySelector('.slide2');
 const slide3 = document.querySelector('.slide3');
-const menu = document.querySelector('.open');
+const right = document.querySelector('.right');
+const left = document.querySelector('.left');
+const right2 = document.querySelector('.right2');
+const left2 = document.querySelector('.left2');
+const right3 = document.querySelector('.right3');
+const left3 = document.querySelector('.left3');
+const closeM = document.querySelector('.open');
+const menu = document.querySelector('header img');
+let count = 1;
 
 slide2.classList.add("hidden");
 slide3.classList.add("hidden");
+closeM.classList.add("hidden");
 
+// * Events
+closeM.addEventListener('click',close);
+menu.addEventListener('click', open);
+right.addEventListener('click', rightCounter);
+right2.addEventListener('click', rightCounter);
+right3.addEventListener('click', rightCounter);
+left.addEventListener('click', leftCounter);
+left2.addEventListener('click', leftCounter);
+left3.addEventListener('click', leftCounter);
+
+// * Function
+function rightCounter(){
+    if(count < 3){
+        count += 1;
+    }
+    else if (count == 3){
+        count = 1;
+    }
+    console.log(count);
+}
 function rightSwap(){
-    slide1.classList.add("hidden");
-    slide2.classList.remove("hidden");
+    if(count == 1){// * at 1
+        slide2.classList.remove("hidden");
+        slide1.classList.add("hidden");
+    }
+    if(count == 2 ){// * at 2
+        slide3.classList.remove("hidden");
+        slide2.classList.add("hidden");
+    }
+    if(count == 3){// * at 3
+        slide1.classList.remove("hidden");
+        slide3.classList.add("hidden");
+    }
+}
+function leftCounter(){
+    if(count <= 3 && count > 1){
+        count -= 1;
+    }
+    else{
+        count = 3;
+    }
+    console.log(count);
 }
 function leftSwap(){
-    slide1.classList.remove("hidden");
-    slide2.classList.add("hidden");
+    if(count == 1){// * at 1
+        slide3.classList.remove("hidden");
+        slide1.classList.add("hidden");
+    }
+    if(count == 2 ){// * at 2
+        slide1.classList.remove("hidden");
+        slide2.classList.add("hidden");
+    }
+    if(count == 3){// * at 3
+        slide2.classList.remove("hidden");
+        slide3.classList.add("hidden");
+    }
 }
 function close(){
-    menu.style.display = "none";
+    closeM.classList.add("hidden");
     console.log('hi');
+}
+function open(){
+    closeM.classList.remove("hidden");
 }
